83418c3ca90170787d57afd8652fa64e0140c80a
yury <yury.antonyb@gmail.com>
2023-04-02 15:53:17 -0400

    criado rotas e spawn de carros

--- OutdoorsProject/GameScene.swift
+++ OutdoorsProject/GameScene.swift
@@ -34,8 +34,20 @@ class GameScene: SKScene {
         let scaleX = backgroundNode.size.width / view.frame.size.width
         let scaleY = backgroundNode.size.height / view.frame.size.height
         cameraNode.setScale(min(scaleX, scaleY))
+        
+        setRoutes()
+        
+        for (index, _) in RoutePathManager.allPaths.enumerated() {
+            let routeName = "path\(index + 1)"
+            let spawnAction = SKAction.run { [weak self] in
+                self?.spawnCarOnRoute(routeName: routeName, duration: 5)
+            }
+            let waitAction = SKAction.wait(forDuration: 2)
+            let sequence = SKAction.sequence([spawnAction, waitAction])
+            let repeatForever = SKAction.repeatForever(sequence)
+            run(repeatForever, withKey: "spawnCarsOn\(routeName)")
+        }
     }
-    
     
     func touchDown(atPoint pos : CGPoint) {
     }
@@ -47,6 +59,8 @@ class GameScene: SKScene {
     }
     
     override func mouseDown(with event: NSEvent) {
+        let location = event.location(in: self)
+        printCoordinates(at: location)
         toggleZoom()
     }
     
@@ -72,5 +86,53 @@ class GameScene: SKScene {
             cameraNode.run(zoomInAction)
         }
         isZoomedIn.toggle()
+    }
+    
+    func drawRoute(path: CGPath) -> SKShapeNode {
+        let route = SKShapeNode(path: path)
+        route.strokeColor = .red
+        route.lineWidth = 5
+        
+        return route
+    }
+    
+    func printCoordinates(at location: CGPoint) {
+        print("Clicado em X: \(location.x), Y: \(location.y)")
+    }
+    
+    func setRoutes() {
+        for (index, path) in RoutePathManager.allPaths.enumerated() {
+            let routeName = "path\(index + 1)"
+            let route = createRoute(points: path, routeName: routeName)
+            addChild(route)
+        }
+    }
+    
+    func createRoute(points: [CGPoint], routeName: String) -> SKShapeNode {
+        let path = CGMutablePath()
+        path.move(to: points[0])
+        
+        for point in points.dropFirst() {
+            path.addLine(to: point)
+        }
+        
+        let route = SKShapeNode(path: path)
+        route.strokeColor = .orange
+        route.lineWidth = 5
+        route.name = routeName
+        return route
+    }
+    
+    func spawnCarOnRoute(routeName: String, duration: TimeInterval) {
+        guard let route = childNode(withName: routeName) as? SKShapeNode, let path = route.path else { return }
+        
+        let car = SKSpriteNode(color: .blue, size: CGSize(width: 20, height: 20))
+        car.position = path.currentPoint
+        addChild(car)
+        
+        let followPath = SKAction.follow(path, asOffset: false, orientToPath: true, duration: duration)
+        let removeCar = SKAction.removeFromParent()
+        let sequence = SKAction.sequence([followPath, removeCar])
+        car.run(sequence)
     }
 }
